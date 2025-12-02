0c081914950e42b0fd833a1bf6750805984341fa
Lincoln Farkas <farkasl27@ucfsd.net>
2025-12-02 22:35:42 +0000

    Fix day 1 part 2

--- src/day01/part2.rs
+++ src/day01/part2.rs
@@ -1,48 +1,43 @@
-// use std::fs::File;
-// use std::io::{self, BufRead};
+use std::fs::File;
+use std::io::{self, BufRead};
 
+fn main() {
+    let mut pos: i32 = 50;
+    let mut count: u32 = 0;
 
-// fn main() {
-//     let mut pos: i32 = 50;
-//     let mut last_pos = pos;
-//     let mut count: u32 = 0;
+    let input_lines = io::BufReader::new(File::open("src/day01/input.txt").unwrap()).lines();
 
-//     let input_lines = io::BufReader::new(File::open("src/day01/sample-input.txt").unwrap()).lines();
+    for line in input_lines.map_while(Result::ok) {
+        let line: &str = line.trim();
+        let mut left = false;
 
-//     for line in input_lines.map_while(Result::ok) {
-//         let line: &str = line.trim();
+        let num = line[1..].parse::<i32>().unwrap();
+        if &line[..1] == "L" {
+            left = true;
+        }
 
-//         let mut num: i32 = line[1..].parse::<i32>().unwrap();
-//         if &line[..1] == "L" {
-//             num = -num;
-//         }
+        if left {
+            if num >= pos {
+                count += if pos != 0 { u32::try_from(1 + (num - pos) / 100).unwrap() } else { u32::try_from(num / 100).unwrap() };
+            }
 
-//         pos += num;
+            pos -= num;
 
-//         while pos > 99 {
-//             pos -= 100;
-//             if pos != 0 && last_pos != 0 {
-//                 count += 1;
-//                 println!("1..{}", line);
-//             }
-//             last_pos = pos;
-//         }
-//         while pos < 0 {
-//             pos += 100;
-//             if pos != 0 && last_pos != 0 {
-//                 count += 1;
-//                 println!("2..{}", line);
-//             }
-//             last_pos = pos;
-//         }
+            while pos < 0 {
+                pos += 100;
+            }
+        } else {
+            if num >= 100 - pos {
+                count += if pos != 0 { u32::try_from(1 + (num - (100 - pos)) / 100).unwrap() } else { u32::try_from(num / 100).unwrap() };
+            }
 
-//         if pos == 0 {
-//             count += 1;
-//             println!("3..{}", line);
-//         }
+            pos += num;
 
-//         last_pos = pos;
-//     }
+            while pos > 99 {
+                pos -= 100;
+            }
+        }
+    }
 
-//     println!("{}..{}", count, last_pos);
-// }
+    println!("{}", count);
+}
