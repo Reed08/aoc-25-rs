75b8406275c5874f93822380704b13f4bea30f1a
Lincoln Farkas <farkasl27@ucfsd.net>
2025-12-04 05:50:57 +0000

    Complete day 4

---
+++ src/day04/part1.rs
@@ -0,0 +1,66 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let mut possible: u32 = 0;
+    let mut map: Vec<Vec<char>> = read_to_string("src/day04/input.txt")
+        .unwrap()
+        .lines()
+        .map(|s| s.chars().collect())
+        .collect();
+
+    for row in 0..map.len() {
+        for col in 0..map[row].len() {
+            if map[row][col] == '.' {
+                continue;
+            }
+            let mut surrounding_count: u8 = 0;
+
+            if row != 0 && (map[row - 1][col] == '@' || map[row - 1][col] == 'x') {
+                surrounding_count += 1;
+            }
+            if row != 0
+                && col + 1 < map[row].len()
+                && (map[row - 1][col + 1] == '@' || map[row - 1][col + 1] == 'x')
+            {
+                surrounding_count += 1;
+            }
+            if col + 1 < map[row].len() && (map[row][col + 1] == '@' || map[row][col + 1] == 'x') {
+                surrounding_count += 1;
+            }
+            if row + 1 < map.len()
+                && col + 1 < map[row].len()
+                && (map[row + 1][col + 1] == '@' || map[row + 1][col + 1] == 'x')
+            {
+                surrounding_count += 1;
+            }
+            if row + 1 < map.len() && (map[row + 1][col] == '@' || map[row + 1][col] == 'x') {
+                surrounding_count += 1;
+            }
+            if row + 1 < map.len()
+                && col != 0
+                && (map[row + 1][col - 1] == '@' || map[row + 1][col - 1] == 'x')
+            {
+                surrounding_count += 1;
+            }
+            if col != 0 && (map[row][col - 1] == '@' || map[row][col - 1] == 'x') {
+                surrounding_count += 1;
+            }
+            if row != 0
+                && col != 0
+                && (map[row - 1][col - 1] == '@' || map[row - 1][col - 1] == 'x')
+            {
+                surrounding_count += 1;
+            }
+
+            if surrounding_count < 4 {
+                possible += 1;
+                map[row][col] = 'x';
+            }
+        }
+    }
+
+    // for line in map {
+    //     println!("{:?}", line);
+    // }
+    println!("{}", possible);
+}
